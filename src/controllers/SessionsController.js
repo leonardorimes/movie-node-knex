97d21a0abcb867582864878dd0118b18e767a504
Leonardo <rimesleo@gmail.com>
2023-04-09 18:16:54 -0300

    adicionando token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,6 +2,13 @@
 const knex = require("../database/knex");
 // importanto o tratamento de exceções
 const AppError = require("../utils/AppError");
+// para descriptografar 
+const { compare } = require("bcryptjs");
+// importando o auth
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken")
+
+
 
 class SessionsController {
     async create(request, response){
@@ -13,7 +20,23 @@ class SessionsController {
             throw new AppError(" Email e/ou senha incorreta", 401)
         }
 
-        return response.json({ user });
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched){
+            throw new AppError("E-mail e/ou senha incorreta", 401)
+        }
+
+        const { secret, expiresIn} = authConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+    
+
+
+        return response.json({ user, token });
     }
 }
 
